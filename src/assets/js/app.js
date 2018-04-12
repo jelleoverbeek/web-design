baf2aa3fee6fcbd4d8c5d69c55339ee8c9c64cf6
Jelle Overbeek <j.overbeek@recognize.nl>
2018-04-12 21:02:40 +0200

    added TMDb api and refresh button now works

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,17 +1,78 @@
 (function () {
 
+    const api = {
+        uri: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + config.TMDb_apikey + "",
+        getMovies: function () {
+            // Source: https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+            fetch(this.uri).then(function(response) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    movies.setMovies(data);
+                    movies.refreshFinished();
+                });
+            }).catch(function(err) {
+                console.log('Fetch Error', err);
+            });
+        }
+    };
+
     const movies = {
-        elements: document.querySelectorAll("article"),
-        toggle: function (el) {
+        getMovieElements: function () {
+            return document.querySelectorAll(".movie");
+        },
+        refreshBtn: document.querySelector(".btn-refresh"),
+        makeTemplate: function(title, releaseDate, img) {
+            const _this = this;
+
+            const html = `<div class="poster">
+                            <img src="//image.tmdb.org/t/p/w500/${img}" alt="">
+                           </div>
+                           <h3>${title} <time>(${releaseDate})</time></h3>
+                           <p>Animation, Comedy</p>`;
+
+            let newMovie = document.createElement('article', { className : 'movie' })
+
+            newMovie.className = "movie";
+            newMovie.insertAdjacentHTML("afterbegin", html);
+            newMovie.addEventListener("click", function (ev) {
+                _this.toggleActive(this)
+            });
+
+            return newMovie
+        },
+        getLastMovie: function() {
+            let movies = this.getMovieElements();
+            return movies[movies.length - 1];
+        },
+        setMovies: function(movies) {
+            const _this = this;
+
+            movies.results.forEach(function (movie) {
+                let newMovie = _this.makeTemplate(movie.title, movie.release_date, movie.poster_path);
+                _this.getLastMovie().insertAdjacentElement("afterend", newMovie);
+            });
+        },
+        toggleActive: function (el) {
             el.classList.toggle("active");
+        },
+        refreshFinished: function() {
+            this.refreshBtn.classList.remove("loading");
+        },
+        refresh: function (el) {
+            el.classList.add("loading");
+            api.getMovies();
         },
         init: function () {
             const _this = this;
 
-            this.elements.forEach(function (article) {
-                article.addEventListener("click", function (ev) {
-                    _this.toggle(this)
-                })
+            this.refreshBtn.addEventListener("click", function (ev) {
+                _this.refresh(this);
+            });
+
+            this.getMovieElements().forEach(function (movie) {
+                movie.addEventListener("click", function (ev) {
+                    _this.toggleActive(this)
+                });
             })
         }
     };
