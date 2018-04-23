90ede36aa02d0d8e3f1671514e7c4819db24e12e
Jelle Overbeek <j.overbeek@recognize.nl>
2018-04-23 11:36:53 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const twig = require('gulp-twig');
+const webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+    return gulp.src('./build/')
+        .pipe(webserver({
+            host: 'localhost',
+            port: 3000,
+            livereload: true,
+            open: true,
+            fallback: './build/index.html'
+        }));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/assets/css'));
+});
+
+
+gulp.task('img', function () {
+    return gulp.src('./src/assets/img/**/*')
+        .pipe(gulp.dest('./build/assets/img'))
+});
+
+gulp.task('js', function () {
+    return gulp.src('./src/assets/js/**/*.js')
+        .pipe(gulp.dest('./build/assets/js'))
+});
+
+gulp.task('twig', function () {
+    const files = [
+        "./src/views/index.twig"
+    ];
+
+    return gulp.src(files)
+        .pipe(twig())
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/assets/**/*', ['default']);
+});
+
+gulp.task('default', ['sass', 'js', 'img', 'twig']);
